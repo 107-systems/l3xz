5bd8ba256af8a08a6352c23329907de43ee918af
Alexander Entinger <consulting@lxrobotics.com>
2023-02-23 09:51:15 +0100

    Update launch file to renaming of ros2_dynamixel_bridge. (#42)

--- launch/robot.py
+++ launch/robot.py
@@ -4,13 +4,13 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    l3xz_ros_dynamixel_bridge_launch_dir = get_package_share_directory('l3xz_ros_dynamixel_bridge') + '/launch/bridge.py'
-    l3xz_ros_dynamixel_bridge = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_ros_dynamixel_bridge_launch_dir))
+    ros2_dynamixel_bridge_launch_dir = get_package_share_directory('ros2_dynamixel_bridge') + '/launch/bridge.py'
+    ros2_dynamixel_bridge = IncludeLaunchDescription(PythonLaunchDescriptionSource(ros2_dynamixel_bridge_launch_dir))
 
     l3xz_head_ctrl_launch_dir = get_package_share_directory('l3xz_head_ctrl')  + '/launch/head_ctrl.py'
     l3xz_head_ctrl = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_head_ctrl_launch_dir))
 
     return LaunchDescription([
-        l3xz_ros_dynamixel_bridge,
+        ros2_dynamixel_bridge,
         l3xz_head_ctrl
     ])
