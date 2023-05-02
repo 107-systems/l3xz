0017c9c558f0f19c9ea62bd92ee5cd3140ea7084
Alexander Entinger <consulting@lxrobotics.com>
2023-05-02 08:53:58 +0200

    Check if CYPHAL_PATH has been set, print an error otherwise.

--- setup_yakut.sh
+++ setup_yakut.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+if [[ ! -v CYPHAL_PATH ]]; then
+    echo "CYPHAL_PATH is not set"
+elif [[ -z "$CYPHAL_PATH" ]]; then
+    echo "CYPHAL_PATH is set to the empty string"
+else
+    echo "CYPHAL_PATH has the value: $CYPHAL_PATH"
+fi
 export UAVCAN__CAN__IFACE='socketcan:can0'
 export UAVCAN__CAN__MTU=8
 export UAVCAN__NODE__ID=$(yakut accommodate)  # Pick an unoccupied node-ID automatically for this shell session.
