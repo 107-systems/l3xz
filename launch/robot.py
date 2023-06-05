54c791ef4a9a06700b27d032e293fd2975afde67
Alexander Entinger <consulting@lxrobotics.com>
2023-06-05 16:36:16 +0200

    Launch all relevant files.

--- launch/robot.py
+++ launch/robot.py
@@ -10,19 +10,23 @@ def generate_launch_description():
     ros2_cyphal_bridge_launch_dir = get_package_share_directory('ros2_cyphal_bridge') + '/launch/bridge.py'
     ros2_cyphal_bridge = IncludeLaunchDescription(PythonLaunchDescriptionSource(ros2_cyphal_bridge_launch_dir))
 
+    l3xz_hydraulic_ctrl_launch_dir = get_package_share_directory('l3xz_hydraulic_ctrl')  + '/launch/hydraulic_ctrl.py'
+    l3xz_hydraulic_ctrl = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_hydraulic_ctrl_launch_dir))
+
     l3xz_watchdog_launch_dir = get_package_share_directory('l3xz_watchdog')  + '/launch/watchdog.py'
     l3xz_watchdog = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_watchdog_launch_dir))
 
     l3xz_head_ctrl_launch_dir = get_package_share_directory('l3xz_head_ctrl')  + '/launch/head_ctrl.py'
     l3xz_head_ctrl = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_head_ctrl_launch_dir))
 
-#    l3xz_gait_ctrl_launch_dir = get_package_share_directory('l3xz_gait_ctrl')  + '/launch/gait_ctrl.py'
-#    l3xz_gait_ctrl = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_gait_ctrl_launch_dir))
+    l3xz_gait_ctrl_launch_dir = get_package_share_directory('l3xz_gait_ctrl')  + '/launch/gait_ctrl.py'
+    l3xz_gait_ctrl = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_gait_ctrl_launch_dir))
 
     return LaunchDescription([
         ros2_dynamixel_bridge,
         ros2_cyphal_bridge,
+        l3xz_hydraulic_ctrl,
         l3xz_watchdog,
-        l3xz_head_ctrl
-#        l3xz_gait_ctrl
+        l3xz_head_ctrl,
+        l3xz_gait_ctrl
     ])
